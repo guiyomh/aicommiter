3062f1582bc2547f02f405d79c3f8dd945221162
Guillaume CAMUS <guillaume.camus@gmail.com>
2024-08-27 18:47:05 +0200

    feat(analyze): Add options to force commit message type, scope, issue and language This commit adds options to the `analyze` command to force the type, scope, issue, and language of the generated commit message.
    - `-s` or `--scope`: Forces the scope of the commit.
    - `-t` or `--type`: Forces the type of the commit.
    - `-i` or `--issue`: Adds the issue number of the commit.
    - `-l` or `--language`: Forces the language of the message.
    
    These options can be used to override the defaults determined by the commit message analyzer.
    
    This allows for greater control over the generated commit messages and ensures consistency across projects.

--- cmd/analyze.go
+++ cmd/analyze.go
@@ -14,7 +14,7 @@ import (
 // analyzeCmd represents the analyze command
 
 func NewAnalyzeCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "analyze",
 		Short: "A brief description of your command",
 		Long: `A longer description that spans multiple lines and likely contains examples
@@ -42,9 +42,12 @@ to quickly create a Cobra application.`,
 				cobra.CheckErr(err)
 			}
 
+			options := buildAnalyzeOptions(cmd)
+
 			commitMessage, err := service.CreateCommitMessage(
 				ctx,
 				diff,
+				options...,
 			)
 			if err != nil {
 				cobra.CheckErr(err)
@@ -52,4 +55,34 @@ to quickly create a Cobra application.`,
 			fmt.Printf("Generated Commit Message:\n%s\n%s\n%s\n", commitMessage.Header, commitMessage.Body, commitMessage.Footer)
 		},
 	}
+
+	cmd.Flags().StringP("scope", "s", "", "Force the scope of the commit")
+	cmd.Flags().StringP("type", "t", "", "Force the type of the commit")
+	cmd.Flags().StringP("issue", "i", "", "Add the issue number of the commit")
+	cmd.Flags().StringP("language", "l", "", "Force the language of the message")
+
+	return cmd
+}
+
+func buildAnalyzeOptions(cmd *cobra.Command) []commitmessage.CommitMessageOption {
+	options := make([]commitmessage.CommitMessageOption, 0)
+
+	scope := cmd.Flags().Lookup("scope")
+	if scope != nil {
+		options = append(options, commitmessage.WithScope(scope.Value.String()))
+	}
+	commitType := cmd.Flags().Lookup("type")
+	if commitType != nil {
+		options = append(options, commitmessage.WithType(commitType.Value.String()))
+	}
+	issue := cmd.Flags().Lookup("issue")
+	if issue != nil {
+		options = append(options, commitmessage.WithIssue(issue.Value.String()))
+	}
+	language := cmd.Flags().Lookup("language")
+	if language != nil {
+		options = append(options, commitmessage.WithLanguage(language.Value.String()))
+	}
+
+	return options
 }
